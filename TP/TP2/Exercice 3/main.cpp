9d4686003a3b5738412c9c6568f11cbaaf51deee
DamienVareille <vareille.damien@sfr.fr>
2015-09-25 22:55:24 +0200

    Modif quicksort (prise du temps)

--- TP/TP2/Exercice 3/main.cpp
+++ TP/TP2/Exercice 3/main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <stdio.h>    
+#include <stdlib.h>     
+#include <time.h> 
 #include "exerciceTrois.h"
 using namespace std;
 
 int main()
 {
-	int tab[11] = { -3,5,4,3,9,6,7,0,8,-7,-9 };
-	quickSort(tab, 0, 10);
+	double elapsedTime;
+	clock_t stopTime;
+	clock_t startTime = clock();
+	int tab[150001];
 
-	for (int i = 0; i < 11; i++)
-		cout << tab[i] << endl;
+	for (int i = 0; i < 150000; i++) {
+		int random = rand() % 100000 + 1;
+		tab[i] = random;
+	}
+	quickSort(tab, 0, 150000);
+	stopTime = clock();
+	elapsedTime = (stopTime - startTime) / (CLOCKS_PER_SEC / (double) 1000.0);
+	cout << "Temps : " << elapsedTime << endl;
 }
