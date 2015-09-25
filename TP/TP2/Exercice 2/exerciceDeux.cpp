1e1c9db4eea3255125c841bcaa3999ba9a2ee47d
Sylvain Metayer <sylvain.metayer@etu.unilim.fr>
2015-09-25 17:42:33 +0200

    Préparation TP2

---
+++ TP/TP2/Exercice 2/exerciceDeux.cpp
@@ -0,0 +1,14 @@
+#include "exerciceUn.h"
+
+int controleSaisieEntiere(char *message, int borneMini, int borneMaxi)
+{
+	int resultat;
+	cout << message << endl;
+	cin >> resultat;
+	while (resultat <= borneMini || resultat >= borneMaxi)
+	{
+		cout << "Merci de recommencer votre saisie" << endl;
+		cin >> resultat;
+	}
+	return resultat;
+}
