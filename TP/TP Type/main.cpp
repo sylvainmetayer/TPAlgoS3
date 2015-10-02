5150a9844f31276f8523035a2906a3de8f2e2f7e
Sylvain Metayer <sylvain.metayer@etu.unilim.fr>
2015-10-02 12:53:01 +0200

    Ajout d'un TP Type

---
+++ TP/TP Type/main.cpp
@@ -0,0 +1,12 @@
+#include "fonctions.h"
+
+//Dans le cas ou il n'y a pas besoin du fonctions.h, decommenter les lignes suivantes
+//#include <iostream>
+//#include <stdio.h> //pour les char, car on ne doit pas utiliser de string
+//using namespace std;
+
+/*Point d'entrée de l'application*/
+int main(void)
+{
+	
+}
