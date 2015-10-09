374652582629c5b267cb3a53bdeda883d014b383
DamienVareille <vareille.damien@sfr.fr>
2015-10-09 18:51:41 +0200

    LOL

--- TP/TP4/liste.cpp
+++ TP/TP4/liste.cpp
@@ -70,3 +70,33 @@
 
 
 //PARTIE DAMIEN
+void afficher(Liste l)
+{
+	Maillon *tmp;
+	void rotation(Maillon *p);
+
+	tmp = l;
+
+	do {
+		cout << (*tmp).numero;
+		rotation(tmp);
+	} while (tmp != l);
+}
+
+void supprimer(Liste l)
+{
+	Maillon *tmp;
+	Maillon *aDetruire;
+
+	tmp = l;
+
+	for (int i = 1; i < longueur(l) - 1; i++)
+	{
+		tmp = (*tmp).suivant;
+	}
+
+	aDetruire = (*tmp).suivant;
+	delete aDetruire;
+
+	(*tmp).suivant = l;
+}
--- TP/TP4/main.cpp
+++ TP/TP4/main.cpp
@@ -1,1 +1,13 @@
 #include "liste.h"
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int choix;
+
+	cout << " -- Menu principal -- ";
+	cout << "1. Afficher la liste";
+	cout << "2. "
+}
