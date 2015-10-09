59e539e3753eb72671bdeb9f7895a1e146dc41b3
Sylvain Metayer <sylvain.metayer@etu.unilim.fr>
2015-10-09 17:58:31 +0200

    TP4

---
+++ TP/TP4/liste.h
@@ -0,0 +1,72 @@
+#ifndef FILEATTENTE_H
+#define FILEATTENTE_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef int Element;
+
+typedef struct Maillon {
+	Element numero;
+	Maillon *suivant;
+} Maillon;
+
+typedef Maillon *Liste;
+
+/*
+Cette procédure permet d'initialiser une liste.
+Paramètre de sortie :
+l : la liste initialisée
+Post-condition :
+estVide(l)
+*/
+void init(Liste& l);
+
+/*
+Permet de désinitialiser une liste.
+Paramètres :
+l : la liste à désinitialiser
+Post-condition :
+estVide(l)
+*/
+void desinit(Liste& l);
+
+/*
+Procédure qui permet d'afficher la liste.
+Paramètre :
+l : la liste à afficher
+Pré-condition :
+non(estVide(l))
+*/
+void afficher(const Liste l);
+
+/*
+Fonction qui retourne la longueur de la liste.
+Paramètre :
+l : la liste.
+Retourne la longueur de la liste.
+*/
+int longueur(Liste l);
+
+/*
+Procédure permettant d'inserer un element en "fin" de liste.
+Paramètre :
+l : la liste
+e : l'element à ajouter
+Post-condition :
+longueur(l) = longueur(l) + 1
+*/
+void inserer(Liste& l, const Element e);
+
+/*
+Procédure permettant de supprimer le "dernier" element de la liste
+Paramètre :
+l : la liste
+Post-condition :
+longueur(l) = longueur(l) - 1
+*/
+void supprimer(Liste& l);
+
+#endif
+
